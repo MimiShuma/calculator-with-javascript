b05582f2ed02ad94af2e1d2caca4bf819794eba2
It's Mimi <shuma.ttt@gmail.com>
2024-05-19 17:48:32 +0600

    program to add buttons in 3x4 dimention.

---
+++ calc.js
@@ -0,0 +1,28 @@
+// screen to display
+let screen = document.getElementById("screen");
+
+
+// display digit from 0 to 9
+let buttonPad = document.getElementById("buttonPad");
+let txtNode = null;
+let btn = null;
+let counter = 3;
+
+for (i = 0; i < 10; i++) {
+
+    // i expect to show mutton 3x3 column
+
+    if (i < counter) {
+
+        txtNode = document.createTextNode(i)
+        btn = document.createElement("BUTTON");
+        btn.appendChild(txtNode)
+        buttonPad.appendChild(btn)
+        if (counter - i === 1) {
+            br = document.createElement("BR");
+            buttonPad.appendChild(br);
+            counter = counter + 3;
+        }
+
+    }
+}
